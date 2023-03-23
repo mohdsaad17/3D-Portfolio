6f75485efb74ffd2e400f47b458681d9a79215f5
Saad Shakil <saadshakil171997@gmail.com>
2023-03-23 15:57:40 +0530

    changes in canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -59,7 +59,8 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: false,
+      antialias:false}}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
